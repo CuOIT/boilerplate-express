48292b2271214bd8cd5971ab7285755ddfe28e5a
CuOIT <legendof2k2@gmai.com>
2022-11-17 22:56:48 +0700

    nothing

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (!process.env.DISABLE_XORIGIN) {
     next();
   });
 }
-var port=process.env.port||3000
+var port=process.env.port||3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
 });
